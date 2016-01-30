b68608eea7361e011e5237acc837bb74842375ea
Jill Li <jill.li87@gmail.com>
2016-01-30 16:07:12 +0100

    styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,17 +1,22 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap-sprockets
 //= require turbolinks
+//= require bootstrap-sprockets
 //= require_tree .
+
+
+
+var duke_blue = "#013088";
+
+$(document).ready(function(){
+  $('.nav-contact li')
+    .mouseover(function(){
+      $(this).css('background-color', duke_blue);
+      $("a", this).css('color', 'whiteSmoke');
+
+    })
+    .mouseout(function(){
+      $(this).css('background-color', "");
+      $("a", this).css('color', '');
+    });
+});
