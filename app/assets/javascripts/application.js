81ce095d616f6498701f80ee3245de1fb9355973
Jill Li <jill.li87@gmail.com>
2016-01-30 22:41:17 +0100

    revised js code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,31 +10,13 @@ var duke_blue = "#013088";
 
 $(function(){
   var changBgColor = function(button) {
-    $(button)
-    .mouseover(function(){
-      $(this).css('background-color', duke_blue);
-      $("a", this).css('color', 'whiteSmoke');
-
-    })
-    .mouseout(function(){
-      $(this).css('background-color', "");
-      $("a", this).css('color', '');
+    $(button).hover(function(){
+      $(this).toggleClass('hover');
+    }, function() {
+      $(this).toggleClass('hover');
     });
   };
-
   changBgColor('.nav-contact li');
-  changBgColor('.nav-bar li');  
-
-  // $(nav_button)
-  //   .mouseover(function(){
-  //     $(this).css('background-color', duke_blue);
-  //     $("a", this).css('color', 'whiteSmoke');
-
-  //   })
-  //   .mouseout(function(){
-  //     $(this).css('background-color', "");
-  //     $("a", this).css('color', '');
-  //   });
-
+  changBgColor('.navbar-collapse li');
 
 });
