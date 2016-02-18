0583237618b9e11772a26c641cd56247ae8e5027
Jill Li <jill.li87@gmail.com>
2016-02-18 21:21:46 +0100

    created admin via devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160218174339) do
-
-  create_table "admins", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "username"
-    t.string   "password_digest"
-    t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
+ActiveRecord::Schema.define(version: 20160218202011) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
