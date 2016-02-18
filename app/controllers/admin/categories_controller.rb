76d8464cdd8ae1e0682d9df02b21c9ca3b9d183b
Jill Li <jill.li87@gmail.com>
2016-02-18 17:40:31 +0100

    added category model routes and view

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,46 @@
+class Admin::CategoriesController < AdminController
+  before_action :find_category, only: [:edit, :update, :delete]
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = "A new category was succefully created."
+      redirect_to admin_categories_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      flash[:success] = "A new category was succefully edited."
+      redirect_to admin_categories_path
+    else
+      render :edit
+    end 
+  end
+
+  def delete
+    @category.destroy
+    flash[:success] = "A new category was succefully deleted."
+    redirect_to admin_categories_path
+  end
+
+  private
+    def category_params
+      params.require(:category).permit(:name)
+    end
+    def find_category
+      @category = Category.find(params[:id])
+    end
+end
