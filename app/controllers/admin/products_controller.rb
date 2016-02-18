2fd9fe11f8dcf3e7187e88db818f9e07f5a3b569
Jill Li <jill.li87@gmail.com>
2016-02-18 09:17:10 +0100

    added actions to products

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,48 @@
 class Admin::ProductsController < AdminController
+  before_action :find_product, only: [:show, :edit, :update, :destroy]
   def index
     @products = Product.all.order("updated_at DESC")
   end
+
+  def new
+    @product = Product.new
+    @showcases = Showcase.all
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @showcases = Showcase.all
+    if @product.save
+      flash[:success] = "A product was created successfully."
+      redirect_to admin_products_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @showcases = Showcase.all
+  end
+
+  def update
+    if @product.update(product_params)
+      flash[:success] = "A product was edited successfully."
+      redirect_to admin_products_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to admin_products_path
+  end
+
+  private
+    def product_params
+      params.require(:product).permit(:title, :url, :description, :showcase_id, :carousel)
+    end
+    def find_product
+      @product = Product.find(params[:id])
+    end
 end
