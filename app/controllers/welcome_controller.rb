1a054b6eafd857aaaa5bccd5aecd8870032940bb
Jill Li <jill.li87@gmail.com>
2016-02-19 14:28:31 +0100

    revised showcase

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
     @carousel_imgs = Product.where(carousel: true)
-    @showcases = Showcase.all
+    @showcases = Showcase.all.where(:show_on_landing_page => true)
   end
 end
