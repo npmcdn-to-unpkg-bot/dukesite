3686e6d6cbd2373794d157b64ebb7004e4b55a99
Jill Li <jill.li87@gmail.com>
2016-02-26 13:55:22 +0100

    added category nav

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_action :get_category
   protect_from_forgery with: :exception
+  def get_category
+    @categories = Category.all
+  end
 end
