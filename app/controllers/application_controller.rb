73103176a923bb35b29c7004c3c2b9af09573c26
Jill Li <jill.li87@gmail.com>
2016-02-26 16:55:12 +0100

    styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,12 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  before_action :get_category
+  before_action :get_category, :get_showcase
   protect_from_forgery with: :exception
   def get_category
     @categories = Category.all
+  end
+  def get_showcase
+    @showcases = Showcase.all
   end
 end
