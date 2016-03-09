3750c09e0236e0d24f9bfe2166e1aebd592d9d0e
Jill Li <jill.li87@gmail.com>
2016-03-09 10:12:04 +0100

    subscriber num

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,5 +2,7 @@ class AdminController < ApplicationController
   layout "admin"
   before_action :authenticate_admin!
   def index
+    subscriber_emails = Subscriber.select(:email).uniq
+    @subscriber_num = subscriber_emails.size
   end
 end
