77c308bb867fbf7f7800f9d4d153b3f3bedd2d43
Erik Lichtenstein <lichtel82@web.de>
2016-03-27 23:39:29 +0200

    migration: added 'visible' to table 'social_network_accounts'

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160315153654) do
+ActiveRecord::Schema.define(version: 20160327211806) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -101,8 +101,9 @@ ActiveRecord::Schema.define(version: 20160315153654) do
     t.string   "platform_name"
     t.text     "platform_url"
     t.text     "img_url"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+    t.boolean  "visible",       default: false
   end
 
   create_table "subscribers", force: :cascade do |t|
