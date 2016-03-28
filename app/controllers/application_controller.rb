63ba8df5591b84b0d58dbbf062a05679877f7963
Erik Lichtenstein <lichtel82@web.de>
2016-03-28 22:20:25 +0200

    implemented social network account list in backend
    added routes for social media account controllers,
    created view for social media account list (backend),
    added sample data to db/seeds.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,16 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  before_action :get_category, :get_showcase
+  before_action :get_category, :get_showcase, :get_social_network_account
   protect_from_forgery with: :exception
   def get_category
     @categories = Category.all
   end
   def get_showcase
     @showcases = Showcase.all
+  end
+  def get_social_network_account
+    @social_network_accounts = SocialNetworkAccount.all
   end
 
   def create_subscriber
