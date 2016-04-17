f697a5eb8f4f9428bb2e98ae0bdfa96814e47487
Jill Li <jill.li87@gmail.com>
2016-04-17 13:51:35 +0200

    trivals

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,24 +21,26 @@ $(function(){
   // lookup-item-on-amazon
   $('#lookup-item-on-amazon-form').on('ajax:success', function(event, xhr) {
     $('#lookup-item-on-amazon-msg').html(xhr.response).removeClass('hide');
+    var returned_items_num = xhr.data.length;
     var item = xhr.data;
-    var asin = item.Asin;
-    var title = item.Title;
-    var product_page_url = item.DetailPageURL;
-    var img_url = item.LargeImageURL;
-    var desc = item.Description;
-
-    $('#product_asin').val(asin);
-    $('#product_title').val(title);
-    $('#product_url').val(product_page_url);
-    $('#product_image_url').val(img_url);
-    $('#product_description').val(desc);
-    $('#product-img-url').attr('src', img_url);
-
+    var asin = item.asin;
+    var title = item.title;
+    var product_page_url = item.detail_page_url;
+    var img_url = item.large_image_url;
+    var desc = item.description;
+    if(returned_items_num > 1) { // show multiple returned items in boxes
+      console.log(item);
+    } else { // show the only returned item in the form
+      $('#product_asin').val(asin);
+      $('#product_title').val(title);
+      $('#product_url').val(product_page_url);
+      $('#product_image_url').val(img_url);
+      $('#product_description').val(desc);
+      $('#product-img-url').attr('src', img_url);
+    };
   }).on('ajax:error', function(event, xhr, status) {
     var response = xhr.responseJSON.response;
     $('#lookup-item-on-amazon-msg').html(response).removeClass('hide');
   });
 
 });
-
