49abbdc139994d5becd5260389a61e4ba7a9c19b
Jill Li <jill.li87@gmail.com>
2016-05-03 20:25:56 +0200

    revised migration

--- db/migrate/20160429210638_remove_img_urls_and_images_table.rb
+++ db/migrate/20160429210638_remove_img_urls_and_images_table.rb
@@ -3,9 +3,9 @@ class RemoveImgUrlsAndImagesTable < ActiveRecord::Migration
     remove_column :categories, :img_url
     remove_column :showcases, :feature_img_url
     remove_column :social_network_accounts, :img_url
-    change_column :categories, :image, :string
-    change_column :showcases, :image, :string
-    change_column :social_network_accounts, :image, :string
+    add_column :categories, :image, :string
+    add_column :showcases, :image, :string
+    add_column :social_network_accounts, :image, :string
 
     drop_table :images
   end
