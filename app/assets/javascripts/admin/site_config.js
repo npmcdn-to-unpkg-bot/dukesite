70fbf5ad76677c7a3cc62a5910309172b8da919a
Jill Li <jill.li87@gmail.com>
2016-05-18 11:57:48 +0200

    bind click event after ajax

--- app/assets/javascripts/admin/site_config.js
+++ app/assets/javascripts/admin/site_config.js
@@ -1,17 +1,6 @@
 $(function(){
   $('#new_keyword').on('ajax:success', function(event, xhr) {
-    var url = xhr.url,
-        new_keyword = "<p>\
-                        <span class='label label-default'>"
-                          + xhr.new_keyword + " " +
-                          "<span class='glyphicon glyphicon-remove' \
-                                 aria-hidden='true' \
-                                 id='remove_keyword' \
-                                 data-url=" + url + ">"
-                          "</span>\
-                         </span>\
-                      </p>";
-    $("#seo-list").after(new_keyword);
+    $("#seo-list").load(location.href + " #seo-list");
   }).on('ajax:error', function(event, xhr) {
     var response = xhr.responseJSON.response;
     $('#seo-msg').html(response).toggleClass('hide');
@@ -19,21 +8,20 @@ $(function(){
       $('#seo-msg').toggleClass('hide');
     }, 3000);
   });
-  $('#seo-list #remove-keyword').each(function(){
-    $(this).click(function(){
-      var url = $(this).data('url');
-      $.ajax({
-        context: this,
-        url: url,
-        type: "DELETE",
-        success: function(){
-          $(this).parents('p').toggleClass('hide');
-        },
-        error: function(e, xhr){
-          var response = xhr.responseJSON.response;
-          $('#seo-msg').html(response).toggleClass('hide');
-        }
-      });
+
+  $('body').on('click', '#remove-keyword', function(){
+    var url = $(this).data('url');
+    $.ajax({
+      context: this,
+      url: url,
+      type: "DELETE",
+      success: function(){
+        $(this).parents('p').toggleClass('hide');
+      },
+      error: function(e, xhr){
+        var response = xhr.responseJSON.response;
+        $('#seo-msg').html(response).toggleClass('hide');
+      }
     });
   });
 });
