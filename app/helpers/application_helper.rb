e843b279f6b4671033db5755e8dd3b4e721878f0
Jill Li <jill.li87@gmail.com>
2016-05-22 13:40:29 +0200

    fixed collapse return 'in' when current page equals to the link_path

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,10 @@ module ApplicationHelper
     current_page?(link_path) ? "active" : ""
   end
 
-  def collapse_class(link_path)
-    current_page?(link_path) ? "in" : ""
+  def collapse_class(*link_paths)
+    collapse_status = false
+    link_paths.each { |link_path| collapse_status ||= current_page?(link_path) }    
+    collapse_status ? 'in' : nil
   end
 
   def url_with_protocol(url)
