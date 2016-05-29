6daf2fcc2dce881acbaa525dc72b0ec77c136bbd
Jill Li <jill.li87@gmail.com>
2016-05-29 13:42:47 +0200

    added images to meta-tag go

--- app/controllers/showcases_controller.rb
+++ app/controllers/showcases_controller.rb
@@ -8,11 +8,13 @@ class ShowcasesController < ApplicationController
     # ------------------------------------------------------------------------------
     title       = @showcase.title
     description = @showcase.subtitle
-    image       = @showcase.photo.image.thumb.url if (@showcase.photo.present? && !@showcase.photo.image.url.nil?)
+    image       = @showcase.products.where(published: true)[0..5].map(&:image_url)
+    image       << @showcase.photo.image.thumb.url if (@showcase.photo.present? && !@showcase.photo.image.url.nil?)
     keywords    = @showcase.keywords.map(&:value) if @showcase.keywords.present?
 
     prepare_meta_tags  title:       title,
                        keywords:    keywords,
+                       image:       image,
                        og: { title:       title,
                              description: description,
                              image:       image }
