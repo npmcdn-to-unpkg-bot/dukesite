69794a54580ff26590a307c606f6650f719c6992
Jill Li <jill.li87@gmail.com>
2016-05-31 12:40:44 +0200

    adjusted height for thumbnails & msg-panel; set the uploaded image size bigger

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -8,7 +8,7 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  process resize_to_fit: [1024, 768]
+  process resize_to_fit: [1800, 1024]
 
   version :thumb do
     process resize_to_fit: [200, 200]
