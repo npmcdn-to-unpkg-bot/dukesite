e009475279cada76c59ac280c9bc15cc1a62db74
Jill Li <jill.li87@gmail.com>
2016-06-04 08:02:34 +0200

    require factory_girl_rails in console

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,7 +11,9 @@ Spork.prefork do
   require 'capybara/rspec'
   require 'database_cleaner'
   require 'pry'
-
+  require 'factory_girl_rails'
+  
+  Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
   ActiveRecord::Migration.maintain_test_schema!
 end
 
