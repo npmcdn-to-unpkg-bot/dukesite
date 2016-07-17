a80f06b6f1f31515c389d287a7b23f881b5ee87b
Jill Li <jill.li87@gmail.com>
2016-07-17 17:41:19 +0200

    Clean up views with helper (display_photo_image_tag)

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -6,4 +6,13 @@ module AdminHelper
   def visible_status_btn(p)
     p.visible? ? "btn-primary" : "btn-default"
   end
+
+  def display_photo_image_tag(obj)
+    if !obj.photo.nil? && obj.photo.image.thumb.url.present?
+      img_url = obj.photo.image.url
+    else
+      img_url = "http://thedudeminds.de/images/no_image_available.png"
+    end
+    return image_tag img_url
+  end
 end
