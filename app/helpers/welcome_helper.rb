3814024b71053c42324c8534cfd0a6d4138888c1
Jill Li <jill.li87@gmail.com>
2016-07-18 19:35:54 +0200

    Packed showcase product carousels with helper; added display photo image helper

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -10,4 +10,32 @@ module WelcomeHelper
     content_tag :li, nil, :data=> {target: target, 'slide-to' => slide_to}, class: status
   end
 
+  def display_photo_image_tag(obj, alt = nil)
+    img_url = image_url(obj)
+    return image_tag img_url, alt: alt
+  end
+
+  def image_url(obj)
+    return obj.photo.image.url if image_exist?(obj)
+  end
+
+  def display_showcase_carousels(obj)
+    # First item must be "active"
+    groups = obj.published_products.in_groups_of(4, false)
+    first_group = groups.first
+
+    groups.collect do |group|
+      status = "item"
+      status << " active" if group == first_group
+      content_tag :div, class: status do
+        sanitize render_products(group)
+      end
+    end.join
+  end
+
+  def render_products(array)
+    array.collect do |p|
+      render partial: "product_thumbnail", locals: { p: p }
+    end.join
+  end
 end
