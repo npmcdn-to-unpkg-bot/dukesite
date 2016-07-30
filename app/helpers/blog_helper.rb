84091f36d8d719efe3a9b1e416abf899ba5be97d
Jill Li <jill.li87@gmail.com>
2016-07-30 15:33:18 +0200

    Refactored blog articles with helpers

---
+++ app/helpers/blog_helper.rb
@@ -0,0 +1,29 @@
+module BlogHelper
+  def no_articles?(articles)
+    articles.size == 0 ? true : false
+  end
+
+  def previous_article?(article)
+    previous_article(article).present? ? true : false
+  end
+
+  def next_article?(article)
+    previous_article(article).present? ? true : false
+  end
+
+  def previous_article_title(article)
+    previous_article(article).title.truncate(27, separator: ' ')
+  end
+
+  def next_article_title(article)
+    next_article(article).title.truncate(27, separator: ' ')
+  end
+
+  def previous_article(article)
+    article.previous_article
+  end
+
+  def next_article(article)
+    article.next_article
+  end
+end
