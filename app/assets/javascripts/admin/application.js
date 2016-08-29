d61395bd86dd7ec839d31cfce9a20aee69018a1f
Jill Li <jill.li87@gmail.com>
2016-08-29 14:26:57 +0200

    Removed jquery-fileupload-rails gem

--- app/assets/javascripts/admin/application.js
+++ app/assets/javascripts/admin/application.js
@@ -1,7 +1,6 @@
 //= require jquery
 //= require jquery.turbolinks
 //= require jquery_ujs
-//= require jquery-fileupload/basic-plus
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_directory .
