b100437c87053d7dab11371c46583790cfe6042b
Jill Li <jill.li87@gmail.com>
2016-08-29 11:59:54 +0200

    renamed helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -30,11 +30,11 @@ module ApplicationHelper
     return image_tag img_url if img_url.present?
   end
 
-  def link_to_display_article_image_tag(obj, link_path, options = {})
-    link_to display_article_image(obj, options), link_path
+  def link_to_display_image_tag(obj, link_path, options = {})
+    link_to display_image(obj, options), link_path
   end
 
-  def display_article_image(obj, options={})
+  def display_image(obj, options={})
     img_url = obj.thumb_img_url
     # Use icon image if the article has no image
     if img_url.nil?
