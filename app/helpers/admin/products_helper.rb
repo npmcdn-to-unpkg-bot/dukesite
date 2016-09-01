68e81fa7e270e3b6c86c1de085b55c9c5da954c7
Jill Li <jill.li87@gmail.com>
2016-09-01 11:25:48 +0200

    Converted html to html (admin> product)

--- app/helpers/admin/products_helper.rb
+++ app/helpers/admin/products_helper.rb
@@ -3,4 +3,7 @@ module Admin::ProductsHelper
     product.image_url.nil? ? img_url = 'http://thedudeminds.de/images/no_image_available.png' : img_url = @product.image_url
     image_tag img_url, class: "img-responsive thumbnail", id: "product-img-url"
   end
+  def product_publish_status(product)
+    product.published? ? "Published" : "Unpublished"
+  end
 end
