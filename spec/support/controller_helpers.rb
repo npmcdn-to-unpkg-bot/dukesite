a421009cb03d2201211cd1c3f0ef9770ced16655
Jill Li <jill.li87@gmail.com>
2016-09-02 12:29:37 +0200

    Added sign_in helper for test

---
+++ spec/support/controller_helpers.rb
@@ -0,0 +1,12 @@
+module ControllerHelpers
+  def sign_in
+    admin = FactoryGirl.create(:admin)
+    if admin.nil?
+      allow(request.env['warden']).to receive(:authenticate!).and_throw(:warden, {:scope => :admin})
+      allow(controller).to receive(:current_admin).and_return(nil)
+    else
+      allow(request.env['warden']).to receive(:authenticate!).and_return(admin)
+      allow(controller).to receive(:current_admin).and_return(admin)
+    end
+  end
+end
